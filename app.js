a05d9b617c69a2c671f03ee228a717d240ef08b3
Joey Van Lierop <joey.vanlierop@me.com>
2020-05-05 20:07:13 -0600

    Add placeholder email sending functionality.

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
 
 const PORT = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
+
+app.get("/", (req, res) => res.send("Hey! Listen!"));
+
+app.post("/send", (req, res) => {
+  console.log(`Received send email request. Body: ${JSON.stringify(req.body)}`);
+  // var data = req.body;
+
+  // var smtpTransport = nodemailer.createTransport({
+  //   service: "Gmail",
+  //   port: 465,
+  //   auth: {
+  //     user: "USERNAME",
+  //     pass: "PASSWORD",
+  //   },
+  // });
+
+  // var mailOptions = {
+  //   from: data.email,
+  //   to: "name@example.com",
+  //   subject: "Portfolio - Contact",
+  //   html: `<p>${data.name}</p>
+  //         <p>${data.email}</p>
+  //         <p>${data.message}</p>`,
+  // };
+
+  // smtpTransport.sendMail(mailOptions, (error, response) => {
+  //   if (error) {
+  //     res.send(error);
+  //   } else {
+  //     res.send("Success");
+  //   }
+  //   smtpTransport.close();
+  // });
+
+  setTimeout(() => res.send("Success"), 1500);
+});
