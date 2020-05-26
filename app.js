7033ebdf37faacec25e261983d78aadd3cbf1a1c
Joey Van Lierop <joey.vanlierop@me.com>
2020-05-25 20:46:59 -0600

    Remove /api path

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api", routes);
+app.use("/", routes);
 
 // Begin listening on the predefined port
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
