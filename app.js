4ee6a37342562ece9a624031705cc385b258ccd6
Joey Van Lierop <joey.vanlierop@pureweb.com>
2020-06-05 15:41:47 -0600

    Add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const express = require("express");
-const routes = require("./routes");
+const mongoose = require("mongoose");
+const routes = require("./api/routes");
 
 // Load Environment Variables
 require("dotenv").config();
@@ -17,3 +18,23 @@ app.use("/", routes);
 
 // Begin listening on the predefined port
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+// Connect to mongo database
+mongoose
+  .connect("mongodb://64.225.36.24:27017/csgo", {
+    auth: {
+      authSource: "admin",
+      user: process.env.MONGO_USER,
+      password: process.env.MONGO_PASS,
+    },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database connected");
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
